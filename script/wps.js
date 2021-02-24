2e8084ef824299ab8334ae7bd991c515d2f3535d
siusiu7 <75193245+siusiu7@users.noreply.github.com>
2021-02-24 12:25:16 +0800

    Update wps.js

--- script/wps.js
+++ script/wps.js
@@ -16,30 +16,30 @@ obj = {
   exp: 0,
   level: 3,
   privilege: [
-    { spid: "data_recover", times: 0, expire_time: 1929574244 },
-    { spid: "ocr", times: 0, expire_time: 1929574244 },
-    { spid: "pdf2doc", times: 0, expire_time: 1929574244 },
-    { spid: "pdf_merge", times: 0, expire_time: 1929574244 },
-    { spid: "pdf_sign", times: 0, expire_time: 1929574244 },
-    { spid: "pdf_split", times: 0, expire_time: 1929574244 }
+    { spid: "data_recover", times: 0, expire_time: 4102366485 },
+    { spid: "ocr", times: 0, expire_time: 4102366485 },
+    { spid: "pdf2doc", times: 0, expire_time: 4102366485 },
+    { spid: "pdf_merge", times: 0, expire_time: 4102366485 },
+    { spid: "pdf_sign", times: 0, expire_time: 4102366485 },
+    { spid: "pdf_split", times: 0, expire_time: 4102366485 }
   ],
   result: "ok",
   total_buy: 0,
-  total_cost: -30,
+  total_cost: 0,
   userid: 0000,
   vip: {
     name: "超级会员",
     has_ad: 0,
     memberid: 40,
-    expire_time: 1929574244,
+    expire_time: 4102366485,
     enabled: [
-      { memberid: 40, name: "超级会员", expire_time: 1929574244 },
-      { memberid: 20, name: "WPS会员", expire_time: 1929574244 },
-      { memberid: 12, name: "稻壳会员", expire_time: 1929574244 }
+      { memberid: 40, name: "超级会员", expire_time: 4102366485 },
+      { memberid: 20, name: "WPS会员", expire_time: 4102366485 },
+      { memberid: 12, name: "稻壳会员", expire_time: 4102366485 }
     ]
   },
   wealth: 0,
-  expire_time: 1929574244
+  expire_time: 4102366485
 };
 
 obj.userid = id;
